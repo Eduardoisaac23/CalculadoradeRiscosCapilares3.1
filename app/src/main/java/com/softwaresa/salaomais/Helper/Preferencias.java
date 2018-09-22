55b2695df7f8c416bc0922728308196ff2228a0d
eduardo <edugmendes.facisabh@gmail.com>
2018-09-22 12:23:15 -0300

    APP Calculo de decisão em procedimentos químicos capiláres.

---
+++ app/src/main/java/com/softwaresa/salaomais/Helper/Preferencias.java
@@ -0,0 +1,45 @@
+package com.softwaresa.salaomais.Helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.PorterDuff;
+import android.view.Display;
+
+import com.google.android.gms.flags.impl.SharedPreferencesFactory;
+
+public class Preferencias {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private String NOME_ARQUIVO = "ConfiguracaoFirebase";
+    private int MODE =0;
+    private SharedPreferences.Editor editor;
+
+    private final String CHAVE_IDENTIFICADOR = "identificarUsuariologado";
+    private final String CHAVE_NOME = "nomeUsuarioLogado";
+
+    public Preferencias(Context context) {
+        this.context = context;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO,MODE);
+
+        editor = preferences.edit();
+        editor.apply();
+    }
+
+    public void salvarUsuarioPreferencias(String identificadorUsuario, String nomeUsuario){
+        editor.putString(CHAVE_IDENTIFICADOR, identificadorUsuario);
+        editor.putString(CHAVE_NOME,nomeUsuario);
+        editor.commit();
+    }
+
+    public String getIdentificador(){
+        return preferences.getString(CHAVE_IDENTIFICADOR, null);
+
+    }
+
+    public String getnome(){
+        return preferences.getString(CHAVE_NOME, null);
+
+    }
+
+}
